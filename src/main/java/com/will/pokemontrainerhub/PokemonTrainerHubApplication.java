9dea43f00dd1097cbbb4f28ad057f98a749c24d4
WillBurdett <williamburdett1995@gmail.com>
2022-08-12 13:02:19 +0100

    app properties for mysql added

---
+++ src/main/java/com/will/pokemontrainerhub/PokemonTrainerHubApplication.java
@@ -0,0 +1,13 @@
+package com.will.pokemontrainerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonTrainerHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonTrainerHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/will/pokemontrainerhub/PokemonTrainerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.pokemontrainerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTrainerHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
