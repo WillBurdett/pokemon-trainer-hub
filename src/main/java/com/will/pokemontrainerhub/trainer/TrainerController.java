fd63c682cad272505e1320a208708deee17d70a7
WillBurdett <williamburdett1995@gmail.com>
2022-08-13 15:35:06 +0100

    trainer crud methods complete

--- src/main/java/com/will/pokemontrainerhub/trainer/TrainerController.java
+++ src/main/java/com/will/pokemontrainerhub/trainer/TrainerController.java
@@ -1,12 +1,11 @@
 package com.will.pokemontrainerhub.trainer;
 
+import com.will.pokemontrainerhub.Exceptions.TrainerNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/trainer")
@@ -22,5 +21,26 @@ public class TrainerController {
     @RequestMapping(method = RequestMethod.GET)
     public List<Trainer> getAllTrainers(){
         return trainerService.getAllTrainers();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<Trainer> getTrainerById(@PathVariable Long id) throws TrainerNotFound {
+        return trainerService.getTrainerById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void addTrainer(@RequestBody Trainer Trainer){
+        trainerService.addTrainer(Trainer);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteTrainerById(@PathVariable Long id){
+        trainerService.deleteTrainerById(id);
+    }
+    // TODO: 13/08/2022 Prevent pokemon being deleted when trainers are deleted (check cascade on trainer model) 
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public void updateTrainer(@PathVariable Long id, @RequestBody Trainer Trainer) throws TrainerNotFound {
+        trainerService.updateTrainerById(id, Trainer);
     }
 }
