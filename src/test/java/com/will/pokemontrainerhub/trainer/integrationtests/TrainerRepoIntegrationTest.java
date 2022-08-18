a62b4c61956906b6322ac829433d1eec1cd7111b
WillBurdett <williamburdett1995@gmail.com>
2022-08-18 14:32:01 +0100

    suite of reop integration tests passing

--- src/test/java/com/will/pokemontrainerhub/trainer/integrationtests/TrainerRepoIntegrationTest.java
+++ src/test/java/com/will/pokemontrainerhub/trainer/integrationtests/TrainerRepoIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.will.pokemontrainerhub.trainer.integrationtests;
 
 import com.will.pokemontrainerhub.Enums.Gender;
-import com.will.pokemontrainerhub.pokemon.PokemonRepository;
 import com.will.pokemontrainerhub.trainer.Trainer;
 import com.will.pokemontrainerhub.trainer.TrainerRepository;
 import org.junit.After;
@@ -10,8 +9,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +26,7 @@ import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @RunWith(SpringRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @DataJpaTest
 public class TrainerRepoIntegrationTest {
 
@@ -51,16 +58,35 @@ public class TrainerRepoIntegrationTest {
     }
 
     @Test
-    public void findByName_thenReturnTrainer() {
+    public void findById_thenReturnTrainer() {
         // given
-        Trainer bob = new Trainer("bob", 2, Gender.MALE);
-        entityManager.persistAndFlush(bob);
+        Trainer bob = new Trainer(1L, "bob", 2, Gender.MALE, new ArrayList<>());
+        entityManager.merge(bob);
+        entityManager.flush();
 
+        System.out.println(trainerRepository.findAll());
         // when
         Optional<Trainer> actual = trainerRepository.findById(1L);
 
         // then
-        assertThat(actual.get()).isEqualTo(bob);
+        assertThat(actual.get().getId()).isEqualTo(bob.getId());
+        assertThat(actual.get().getName()).isEqualTo(bob.getName());
+        assertThat(actual.get().getAge()).isEqualTo(bob.getAge());
+        assertThat(actual.get().getGender()).isEqualTo(bob.getGender());
+        // TODO: 18/08/2022 implement a way to comprehensively compare/test lists
+        // assertThat(actual.get().getPokemon()).isEqualTo(bob.getPokemon());
+    }
+
+    @Test
+    public void savesTrainer_UpdatesTrainer() {
+        // given
+        Trainer bob = new Trainer("bob", 2, Gender.MALE);
+
+        // when
+        trainerRepository.save(bob);
+
+        // then
+        assertThat(trainerRepository.findById(1L)).isEqualTo(Optional.of(bob));
     }
 
     @Test
